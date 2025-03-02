1e2b3676a959ada0070a9093062a03bb8c432265
Juan Colapaolo <145474462+jigcolapaolo@users.noreply.github.com>
2025-03-02 01:53:44 -0300

    feat: added url shortener microservice

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser')
+const dns = require('dns')
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -18,6 +22,43 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+let savedUrls = []
+
+app.get('/api/shorturl/:shorturl' ,(req, res) => {
+  const { shorturl } = req.params
+
+  const urlObject = savedUrls.find(u => u.short_url === parseInt(shorturl))
+
+  if (urlObject) res.redirect(urlObject.original_url)
+  else  res.json({ error: "NOT FOUND" })
+})
+
+app.post('/api/shorturl', (req, res) => {
+  const { url } = req.body
+
+  let host
+  try {
+    host = new URL(url).hostname
+  } catch (err) {
+    return res.json({ error: "invalid url" })
+  }
+
+  dns.lookup(host, (err, address, family) => {
+    if (err) return res.json({ error: 'invalid url' })
+
+    const shortUrl = Math.floor(Math.random() * 100)
+      
+    const urlObject = {
+      "original_url": url,
+      "short_url": shortUrl
+    }
+
+    savedUrls.push(urlObject)
+
+    res.json(urlObject)
+  })
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
